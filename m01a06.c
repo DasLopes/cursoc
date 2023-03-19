0d0e2db47c8a4d83fccbc440003ccc67b5d88e02
daslopes <daslopes@outlook.com.br>
2023-03-19 17:16:06 -0300

    anotações aula m01a06

--- m01a06.c
+++ m01a06.c
@@ -1,9 +1,16 @@
 #import <stdio.h>
 #import <locale.h>
-
+#define TOTAL 8 //forma de define uma constante
 void main(){
     setlocale(LC_ALL, "Portuguese");
-
-    int TOTAL = 8;
+    //const int TOTAL = 8; //forma de define uma constante
     printf("%d", TOTAL);
 }
+/*#define VS const
+
+#define é uma diretiva de pré-processamento
+const é uma palavra reservada do C
+Com #define, o valor é substituído antes da compilação
+Com const, a constante existe na memória
+Com const é possível indicar o tipo primitivo da constante
+*/
