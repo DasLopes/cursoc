8f98594ca7860e4e6851cd6a1d3e49bed77c8aae
daslopes <daslopes@outlook.com.br>
2023-03-20 09:26:48 -0300

    resolvendo problema com fflush(stdin);

--- exercicios/EX004 - LISTAGEM.c
+++ exercicios/EX004 - LISTAGEM.c
@@ -2,53 +2,41 @@
 #import <locale.h>
 void main(){
     setlocale(LC_ALL, "Portuguese");
-
-    printf("Cadastrando a primeira pessoa:\n");
+    char nome1[20], nome2[20], nome3[20];
+    char sexo1, sexo2, sexo3;
+    float nota1, nota2, nota3;
+    printf("\nCadastrando a primeira pessoa:\n");
     printf("------------------------------\n");
-
-    char nome1[] = "Robson";
-    //printf("NOME: ");
-    //gets(nome1);
-    char sexo1 = 'M';
-    //printf("SEXO [M/F]: ");
-    //sexo1 = getchar();
-    float nota1 = 6;
-    //printf("NOTA: ");
-    //scanf("%f", &nota1);
-
-    printf("Cadastrando a segunda pessoa:\n");
+    fflush(stdin);
+    printf("NOME: ");
+    gets(nome1);
+    printf("SEXO [M/F]: ");
+    sexo1 = getchar();
+    printf("NOTA: ");
+    scanf("%f", &nota1);
+    printf("\nCadastrando a segunda pessoa:\n");
     printf("------------------------------\n");
-
-    char nome2[] = "Michele";
-    //printf("NOME: ");
-    //gets(nome2);
-    //printf("\n");
-    char sexo2 = 'F';
-    //printf("SEXO [M/F]: ");
-    //sexo2 = getchar();
-    //printf("\n");
-    float nota2 = 7;
-    //printf("NOTA: ");
-    //scanf("%f", &nota2);
-
+    fflush(stdin);
+    printf("NOME: ");
+    gets(nome2);
+    printf("SEXO [M/F]: ");
+    sexo2 = getchar();
+    printf("NOTA: ");
+    scanf("%f", &nota2);
     printf("Cadastrando a terceira pessoa:\n");
     printf("------------------------------\n");
-
-    char nome3[] = "Miguel";
-    //printf("NOME: ");
-    //gets(nome3);
-    char sexo3 = 'M';
-    //printf("SEXO [M/F]: ");
-    //sexo3 = getchar();
-    float nota3 = 8;
-    //printf("NOTA: ");
-    //scanf("%f", &nota3);
-
+    fflush(stdin);
+    printf("NOME: ");
+    gets(nome3);
+    printf("SEXO [M/F]: ");
+    sexo3 = getchar();
+    printf("NOTA: ");
+    scanf("%f", &nota3);
     printf("\n\nListagem completa\n");
     printf("------------------------------\n");
-    printf("NOME\t\tSEXO\tNOTA\n");
-    printf("%s\t\t%c\t%.1f\n", nome1, sexo1, nota1);
-    printf("%s\t\t%c\t%.1f\n", nome2, sexo2, nota2);
-    printf("%s\t\t%c\t%.1f\n", nome3, sexo3, nota3);
+    printf("NOME                SEXO NOTA\n");
+    printf("%-20s %c %6.1f \n", nome1, sexo1, nota1);
+    printf("%-20s %c %6.1f \n", nome2, sexo2, nota2);
+    printf("%-20s %c %6.1f \n", nome3, sexo3, nota3);
     printf("------------------------------\n");
 }
