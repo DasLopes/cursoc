b2ddfd0dec3bef4a84f9b3127573125fc981c5a4
daslopes <daslopes@outlook.com.br>
2023-03-20 08:41:19 -0300

    arquivo renomeado

--- exercicios/EX001.c
+++ exercicios/EX001.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <locale.h>
 void main(){
-    printf("\n\nListagem de Alunos\n");
-    printf("Nome\t\tNota\n");
+    setlocale(LC_ALL, "Portuguese");
+    printf("Listagem de Alunos \n");
+    printf("Nome\t\tNota \n");
     printf("---------------------\n");
     printf("Ana Beatriz\t8.5\n");
     printf("Bianca Martins\t9.0\n");
