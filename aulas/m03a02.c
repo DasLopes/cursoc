05224189131cf010e1bc00a99fbd81529dedde54
daslopes <daslopes@outlook.com.br>
2023-04-16 21:01:16 -0300

    usando biblioteca time

---
+++ aulas/m03a02.c
@@ -0,0 +1,12 @@
+#import <stdio.h>
+#import <time.h>
+void main(){
+    time_t t;
+    time(&t);
+    struct tm *data;
+    data = localtime(&t);
+    int d = data->tm_mday;
+    int m = data->tm_mon + 1;
+    int a = data->tm_year + 1900;
+    printf("Estamos no dia %i e no mes %i e no ano de %i", d, m, a);
+}
